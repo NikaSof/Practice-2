6771aedeaa197a67b93b1ce0db127a9416a17df6
Nika <116892467+NikaSof@users.noreply.github.com>
2023-02-26 18:56:36 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include "Header1.h"
 
-bool tringle::exst_tr(double a, double b, double c) {
+bool tringle::exst_tr() {
 	return ((a + b > c) && (a + c > b) && (b + c > a));
 }
 void tringle::set(double a1,double b1,double c1) {
@@ -11,15 +11,15 @@ void tringle::set(double a1,double b1,double c1) {
 	c = c1;
 }
 void tringle::show(double a, double b, double c) {
-	std::cout << "1 сторона: " << a << endl;
-	std::cout << "2 сторона: " << b << endl;
-	std::cout << "3 сторона: " << c << endl;
+	std::cout << "1 СЃС‚РѕСЂРѕРЅР°: " << a << endl;
+	std::cout << "2 СЃС‚РѕСЂРѕРЅР°: " << b << endl;
+	std::cout << "3 СЃС‚РѕСЂРѕРЅР°: " << c << endl;
 }
-double tringle::perimetr(double a, double b, double c) {
+double tringle::perimetr() {
 	double P = a + b + c;
 	return P;
 }
-double tringle::square(double a, double b, double c) {
+double tringle::square() {
 	double p = (a + b + c) / 2;
 	double S = sqrt(p * (p - a) * (p - b) * (p - c));
 	return S;
